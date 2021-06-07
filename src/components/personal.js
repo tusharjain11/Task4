a60bb213e5a0991bd46c3f824955f50fbf413282
Tushar Jain <60441369+tusharjain11@users.noreply.github.com>
2021-06-07 17:16:17 +0530

    Create personal.js

---
+++ src/components/personal.js
@@ -0,0 +1,44 @@
+import React from "react"
+import "./personal.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+
+const Personal  = () => {
+    return(
+        <div className = "parentContainer">
+            <div className = "top-container">
+                <h4>Batch Name | PHY_B1</h4>
+                <img className = " note "src = "assets/edit.png"></img>
+            </div>
+            <div className = "bodyContainer">
+                <div className = "lineOne">
+                  <img src = "assets/photoSelf.png" alt = "my photo"></img>
+                  <span>Marvin Mckinney</span>
+                  <img src = "assets/add-friend.png" className = "friend"></img>
+                  
+                </div>
+                
+                <div className = "lineTwo">
+                    <img src = "assets/user.png"></img>
+                   <span>125 Students </span>
+                </div>
+                
+                <div className = "lineThree">
+                  <div className = "leftSide">
+                    <img className = "time" src = "assets/time.png"></img>
+                    <div>
+                     <p>Upcoming {`<Activity>`}</p>
+                     <p>Thu, 25th May</p>
+                     <p>12:00 PM - 12:00PM</p>
+                     </div>
+                 </div>
+                 <div className = "rightSide">
+                     <button>View</button>
+                </div> 
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Personal;
